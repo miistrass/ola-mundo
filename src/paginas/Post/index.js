8466fc4ec0e5b3c5230b4ce218fcdd454977f742
Milena Strassburger <milenastrass@gmail.com>
2024-02-03 12:37:19 -0300

    implementando posts recomendados e o scroll to top

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,10 +1,12 @@
 import "./Post.css";
+import styles from "./Post.module.css"
 import { Route, Routes, useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
 import NaoEncontrada from "paginas/NaoEcontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import PostCard from "componentes/PostCard";
 
 export default function Post() {
   const parametros = useParams();
@@ -14,8 +16,13 @@ export default function Post() {
   });
 
   if (!post) {
-    return <NaoEncontrada></NaoEncontrada>;
+    return <NaoEncontrada />;
   }
+
+  const postsRecomendados = posts
+    .filter((post) => post.id !== Number(parametros.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4)
 
   return (
     <Routes>
@@ -28,6 +35,19 @@ export default function Post() {
                 <div className="post-markdown-container">
                   <ReactMarkdown>{post.texto}</ReactMarkdown>
                 </div>
+
+                <h2 className={styles.tituloOutrosPosts}>
+                  Outros posts que você pode gostar:
+                </h2>
+
+                <ul className={styles.postsRecomendados}>
+                  {postsRecomendados.map((post) => (
+                    <li key={post.id}>
+                      <PostCard post={post}/>
+                    </li>
+                  ))}
+                </ul>
+
               </PostModelo>
             } />
         </Route>
